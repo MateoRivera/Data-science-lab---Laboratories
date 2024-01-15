ca691dd1d114bd36dc159f4b5a0942915cd2878d
MateoRivera <62917220+MateoRivera@users.noreply.github.com>
2024-01-15 10:25:03 +0100

    Template for deployment phase

--- Final project/functions.py
+++ Final project/functions.py
@@ -3,3 +3,8 @@ def custom_error(estimator, X_test, y_test):
     from sklearn.metrics.pairwise import euclidean_distances
     y_pred = estimator.predict(X_test)
     return np.mean(np.diag(euclidean_distances(y_test, y_pred)))
+
+def custom_error2(y_true, y_pred):
+    import numpy as np
+    from sklearn.metrics.pairwise import euclidean_distances
+    return np.mean(np.diag(euclidean_distances(y_true, y_pred)))

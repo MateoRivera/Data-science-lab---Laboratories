bd7a01b1774d6ec73d2ca6da52a4cb3a04de2bfc
MateoRivera <62917220+MateoRivera@users.noreply.github.com>
2024-01-16 00:39:08 +0100

    Refinación

--- Final project/functions.py
+++ Final project/functions.py
@@ -9,10 +9,15 @@ def custom_error2(y_true, y_pred):
     from sklearn.metrics.pairwise import euclidean_distances
     return np.mean(np.diag(euclidean_distances(y_true, y_pred)))
 
-def custom_error_for_large_datasets(estimator, X_test, y_test):
+def mean_euclidean_distance_error(estimator, X_test, y_test):
     from scipy.spatial.distance import euclidean
     import numpy as np
     y_pred = estimator.predict(X_test)
+    return np.mean([euclidean(y_test_i, y_pred_i) for y_test_i, y_pred_i in zip(y_test, y_pred)])
+
+def custom_error_for_large_datasets2(y_test, y_pred):
+    from scipy.spatial.distance import euclidean
+    import numpy as np
     return np.mean([euclidean(y_test_i, y_pred_i) for y_test_i, y_pred_i in zip(y_test, y_pred)])
 
 def save_submission(n_submissions, rf_pred):

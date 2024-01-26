1398bbcf18aaebc6cb3e842de7e6148185ec24a4
Mateo Rivera <mateo.rivera@udea.edu.co>
2024-01-26 12:22:55 +0100

    Hice tantos cambios que ya ni se que hice

--- Final project/functions.py
+++ Final project/functions.py
@@ -17,7 +17,7 @@ def save_submission(path, y_pred):
             writer.writerow([i, f"{x}|{y}"])
 
 class RandomizedSearchHO():
-    def __init__(self, estimator, param_distributions, n_iter=10, scoring=None, n_jobs=-1):
+    def __init__(self, estimator, param_distributions, n_iter=10, scoring=None):
         self.__estimator = estimator
         self.__param_distributions = param_distributions
         self.__n_iter = n_iter
@@ -31,6 +31,8 @@ class RandomizedSearchHO():
     def fit(self, X, y):
         from sklearn.base import clone
         from sklearn.model_selection import train_test_split
+        import numpy as np
+        from itertools import product
 
         # Let's split X, y into train and validation sets
         X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=8)
@@ -40,6 +42,7 @@ class RandomizedSearchHO():
 
         # Let's train the models
         for i, param_sample in enumerate(samples_param_grid):
+            print(f"Training model {i+1}/{self.__n_iter}")
             # We clone the estimator because we want to keep the best one
             estimator = clone(self.__estimator)
 
@@ -65,5 +68,21 @@ class RandomizedSearchHO():
                     self.best_params_ = param_sample
                     self.best_estimator_ = estimator
                     self._best_score = current_score
+
+            print(f"Hyperparameters: {param_sample}")
+            print(f"Score: {current_score}")
+            print("--------------------------------------------------"*50)
             
+        return self
+    
+    def predict(self, X):
+        return self.best_estimator_.predict(X)
+    
+class SFSExtractor(object):
+    def __init__(self, features_selected):
+        self.selected_features = features_selected
+    def transform(self, X):
+        return X.loc[:, self.selected_features]
+    
+    def fit(self, X, y=None):
         return self
